780b666076852bfdbad813d06e3d0854d0029e2b
Lascelles <waunaneville@gmail.com>
2026-08-20 23:25:15 +0300

    feat: define KPIS array with portfolio metrics (GLP, PAR, NPL, MTD efficiency, NIM)

--- src/pages/lender/CommandCenter.jsx
+++ src/pages/lender/CommandCenter.jsx
@@ -1,3 +1,10 @@
 
 import { portfolioKpis, parAgingBreakdown, marketClusters } from "../../data/mockLenderData";
 
+const KPIS = [
+  ["Gross Loan Portfolio", portfolioKpis.grossLoanPortfolio],
+  ["PAR > 30 Days", portfolioKpis.parOver30],
+  ["NPL > 90 Days", portfolioKpis.nplOver90],
+  ["MTD Collection Efficiency", portfolioKpis.mtdCollectionEfficiency],
+  ["Net Interest Margin", portfolioKpis.netInterestMargin],
+];
