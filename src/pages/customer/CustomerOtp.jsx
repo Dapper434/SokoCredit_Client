183a42e34aa5841383a21adf3925f21f9b46404f
Lascelles <waunaneville@gmail.com>
2026-08-23 18:47:56 +0300

    feat(auth): add Customer OTP verification screen with phone state handling

---
+++ src/pages/customer/CustomerOtp.jsx
@@ -0,0 +1,8 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import OtpInput from "../../components/otp/OtpInput";
+import CustomerAuthFrame from "../../components/layout/CustomerAuthFrame";
+
+export default function CustomerOtp() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const phone = location.state?.phone || "07XX XXX XXX";
