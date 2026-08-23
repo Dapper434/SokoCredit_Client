d8321bc828fc11ef6ed887a2c172e44bd7dd643b
Lascelles <waunaneville@gmail.com>
2026-08-23 16:44:30 +0300

    feat: integrate staff roster and change request data with StatusBadge component

---
+++ src/pages/lender/StaffAccess.jsx
@@ -0,0 +1,3 @@
+import { useState } from "react";
+import { staffRoster, changeRequests, changeableFields, allMarkets } from "../../data/mockLenderData";
+import StatusBadge from "../../components/shared/StatusBadge";
