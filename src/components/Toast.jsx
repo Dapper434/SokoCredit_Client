0fd6f550be22f6d13bf4c4c33fa830e83f663d58
Dapper434 <gpflaslik7112@gmail.com>
2026-08-25 14:02:42 +0300

    feat:(shared) scaffold Toast.jsx wth useToast hook signature

---
+++ src/components/Toast.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect, useCallback } from "react";
+
+export function useToast() {
+
+  
+
+
+  function ToastContainer() {
+    return null;
+  }
+
+  return { toast: () => {}, ToastContainer };
+}
