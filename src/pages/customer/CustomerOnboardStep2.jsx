7a038227c6c9080c74c2e3781dd2093b5dc89ed6
Lascelles <waunaneville@gmail.com>
2026-08-25 16:24:36 +0300

    feat(onboarding): add SVG back arrow icon to Step2 navigation button

--- src/pages/customer/CustomerOnboardStep2.jsx
+++ src/pages/customer/CustomerOnboardStep2.jsx
@@ -31,3 +31,6 @@ export default function CustomerOnboardStep2() {
             onClick={() => navigate("/customer/onboarding/1")}
             className="text-white/70 text-sm flex items-center gap-1.5 hover:text-white"
           ></button>
+            <svg viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+              <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
+            </svg>
